fa51f9225084a61398ac60a727d97e1542e54a4a
btlk <ivanmitrafanych@gmail.com>
2017-02-27 22:16:31 +0300

    Enh: few additions to package metadata, registered pypi

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ from setuptools import setup, find_packages
 reqs = [x.strip() for x in open('REQUIREMENTS.txt', 'r').readlines()]
 
 setup(
-  name="siam",
-  version="0.1",
+  name='siam',
+  version='0.1',
   packages=find_packages('src'),
-  package_dir={'': 'src'},
+  # package_dir={'': 'src'},
   scripts=[],
 
   install_requires=reqs,
 
-  author="Alexander 'btlk' Serikov",
-  author_email="ivanmitrafanych@gmail.com",
-  description="Tensorflow implementation of siamese image comparing net",
-  license="MIT",
-  keywords="tensorflow neural network convolution siamese image"
+  author='Alexander \'btlk\' Serikov',
+  author_email='ivanmitrafanych@gmail.com',
+  description='Tensorflow implementation of siamese image comparing net',
+  download_url='https://github.com/btlk/siam/',
+  license='MIT',
+  keywords=['tensorflow', 'neural', 'network', 'convolution', 'siamese', 'image']
 )
