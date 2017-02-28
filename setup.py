64ca239800f12b5456ad1fc6347f749deefa7541
btlk <ivanmitrafanych@gmail.com>
2017-02-28 22:23:30 +0300

    New: bin/main.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
 
+import os
+
 reqs = [x.strip() for x in open('REQUIREMENTS.txt', 'r').readlines()]
 
+scripts = [os.path.join(d, x)
+           for d, dirs, files in os.walk('bin')
+           for x in files if x.endswith('.py')]
+scripts_dir = os.path.expanduser('~/siam/bin')
+if not os.path.exists(scripts_dir):
+    os.makedirs(scripts_dir)
 
 setup(
   name='siam',
   version='0.1',
   packages=['siam'],
   package_dir={'siam': 'src'},
-  scripts=[],
+  data_files=[(scripts_dir, scripts)],
 
   install_requires=reqs,
 
---
+++ src/net.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+
+def loss_function(net_out):
+  pass
+
+def construct_net():
+  pass
